95cc66e9d0820acae0fd7870421e4f887b68a24f
Markus Afonso <mafonso4@my.bcit.ca>
2022-11-22 14:56:00 -0800

    button work better

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,10 @@ const clickButton = document.querySelector('.click-me')
 // console.log(clickButton)
 
 function printHello() {
+    // window.open("https://www.w3schools.com")
     alert('Works')
+    // clickButton.removeEventListener('click', printHello) //Removes so it stops
 }
 
-clickButton.addEventListener('click', printHello)   
+clickButton.addEventListener('click', printHello, {once:true})
+
