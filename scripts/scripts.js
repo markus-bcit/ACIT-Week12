0e559e89c4b3d18b21450540d2f077e3808fe329
Markus Afonso <mafonso4@my.bcit.ca>
2022-11-22 15:22:36 -0800

    background pink

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,15 @@ const clickButton = document.querySelector('.click-me')
 // console.log(clickButton)
 
 function printHello() {
-    // window.open("https://www.w3schools.com")
+    window.open("https://www.w3schools.com")
     alert('Works')
     // clickButton.removeEventListener('click', printHello) //Removes so it stops
 }
 
 clickButton.addEventListener('click', printHello, {once:true})
 
+function changeBGPink()
+{
+    document.body.classList.add("backgroundPink")
+}
+clickButton.addEventListener('click', changeBGPink)
