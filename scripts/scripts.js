8a41efd390e36ff10f2869e9f461d2d7edc64c1b
Markus Afonso <mafonso4@my.bcit.ca>
2022-11-22 15:56:40 -0800

    adds image on button click

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,15 +2,39 @@ const clickButton = document.querySelector('.click-me')
 // console.log(clickButton)
 
 function printHello() {
-    window.open("https://www.w3schools.com")
+    // window.open("https://www.w3schools.com")
     alert('Works')
     // clickButton.removeEventListener('click', printHello) //Removes so it stops
 }
 
 clickButton.addEventListener('click', printHello, {once:true})
-
+// 
 function changeBGPink()
 {
     document.body.classList.add("backgroundPink")
 }
+
 clickButton.addEventListener('click', changeBGPink)
+
+// Changes txt content on click 
+function changetext(){
+    if (clickButton.textContent === 'click me'){
+        clickButton.textContent = "Clicked!"
+    }
+    else clickButton.textContent = "click me"
+    }
+
+clickButton.addEventListener('click', changetext)
+
+// Adds image on button press
+function updateimg(){
+    const img = document.getElementById("shoppingCart")
+    img.src = "images/263142.png"
+    img.width = '100'
+}
+
+clickButton.addEventListener('click', updateimg)
+
+
+
+
